4565416c9e7dbfc141f51ad2bf7d9a2e8ec73f6d
kuzmaarek1 <95350850+kuzmaarek1@users.noreply.github.com>
2025-02-10 01:15:26 +0100

    Add a progress bar

--- app/components/CustomButton.tsx
+++ app/components/CustomButton.tsx
@@ -1,11 +1,6 @@
-import React, { useState, useEffect } from "react";
-import {
-  Animated,
-  Text,
-  View,
-  TouchableOpacity,
-  useColorScheme,
-} from "react-native";
+import React from "react";
+import { Text, View, TouchableOpacity, useColorScheme } from "react-native";
+import * as Progress from "react-native-progress";
 
 interface CustomButtonProps {
   title: string;
@@ -21,56 +16,52 @@ const CustomButton = ({
   progressPercent,
 }: CustomButtonProps) => {
   const scheme = useColorScheme();
-  const [fillAnimation] = useState(new Animated.Value(0));
 
-  useEffect(() => {
-    if (isLoading) {
-      Animated.timing(fillAnimation, {
-        toValue: progressPercent,
-        duration: 500,
-        useNativeDriver: false,
-      }).start();
-    }
-  }, [isLoading, progressPercent]);
+  const buttonBgColor = scheme === "dark" ? "bg-white" : "bg-mediumGrey";
+  const borderColor = scheme === "dark" ? "border-grey" : "border-darker";
+  console.log(`progressPercent: ${progressPercent}`);
 
-  const fillWidth = fillAnimation.interpolate({
-    inputRange: [0, 100],
-    outputRange: ["0%", "100%"],
-  });
-
-  const buttonBgColor =
-    scheme === "dark"
-      ? isLoading
-        ? "bg-mediumGrey"
-        : "bg-white"
-      : isLoading
-      ? "bg-white"
-      : "bg-mediumGrey";
-
-  const borderColor = scheme === "dark" ? "border-grey" : "border-mediumGrey";
   return (
     <View
       className={`flex items-center justify-center mt-8 ${
         scheme === "dark" ? "text-mediumGrey" : "text-white"
       }`}
     >
-      <TouchableOpacity
-        className={`relative w-48 h-12 justify-center items-center border-2 ${borderColor} ${buttonBgColor}`}
-        onPress={onPress}
-        disabled={isLoading}
-      >
-        <Animated.View
-          className="absolute top-0 left-0 bottom-0"
-          style={{ width: fillWidth }}
-        />
-        <Text
-          className={`font-bold text-lg ${
-            scheme === "dark" ? "text-mediumGrey" : "text-white"
-          }`}
+      {!isLoading ? (
+        <TouchableOpacity
+          className={`relative w-[165px] h-[50px] justify-center items-center border-[1px] rounded-[10px] ${borderColor} ${buttonBgColor}`}
+          onPress={onPress}
+          disabled={isLoading}
         >
-          {isLoading ? `${progressPercent}%` : title}
-        </Text>
-      </TouchableOpacity>
+          <Text
+            className={`font-bold text-lg ${
+              scheme === "dark" ? "text-mediumGrey" : "text-white"
+            }`}
+          >
+            {title}
+          </Text>
+        </TouchableOpacity>
+      ) : (
+        <View className="relative w-[165px]">
+          <Progress.Bar
+            progress={progressPercent / 100}
+            width={165}
+            height={48}
+            borderRadius={10}
+            borderWidth={1}
+            borderColor={scheme === "dark" ? "#8d8686" : "#212121"}
+            color={scheme === "dark" ? "#ffffff" : "#3d3d3d"}
+            indeterminate={
+              progressPercent === 0 || progressPercent === undefined
+            }
+          />
+          <Text
+            className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-[16px] font-bold text-darker`}
+          >
+            {progressPercent.toFixed(1)}%
+          </Text>
+        </View>
+      )}
     </View>
   );
 };
